fa63435476feff0f6fef492e58d42ec5846095b6
Matija Skrletović <skrlama@gmail.com>
2023-12-14 20:30:31 +0100

    Definiranje kodne stranice

--- edunovawp3.sql
+++ edunovawp3.sql
@@ -3,7 +3,7 @@ go
 drop database if exists edunovawp3;
 go
 -- ovo je komentar
-create database edunovawp3;
+create database edunovawp3 collate Croatian_CI_AS;
 go
 --drop database edunovawp3;
 --use master
@@ -12,12 +12,10 @@ use edunovawp3;
 create table smjerovi(
 sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
-trajanje int null,  -- ako ne piše not null onda se podrazumijeva da je null
-cijena decimal(18,2),
+trajanje int null, --null se ne piše. Ako ne piše not null onda se podrazumjeva null
+cijena decimal(18,2), -- iako ništa ne piše je null
 vaucer bit
 );
-
-ovo je dio koji sam promijenio da bi mi github to vidio
 
 create table grupe(
 
@@ -32,19 +30,19 @@ create table grupe(
 
 
 create table polaznici(
-sifra int				not null primary key identity(1,1),
-ime varchar(50)			not null,
-prezime varchar(50)		not null,
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
 email varchar(100),
 oib char(11),
 brojugovora varchar(10)
 );
 
 create table predavaci(
-sifra int			not null primary key identity(1,1),
-ime varchar(50)		not null,
-prezime varchar(50)	not null,
-email varchar(100)	not null,
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+email varchar(100) not null,
 oib char(11),
 iban varchar(50)
 );
@@ -58,13 +56,14 @@ grupa int not null,
 polaznik int not null
 );
 
-
---kreiranje vanjskih ključeva
+-- kreiranje vanjskih ključeva
 alter table grupe add foreign key (smjer) references smjerovi(sifra);
-alter table grupe add foreign key (predavac) references predavaci (sifra);
+alter table grupe add foreign key (predavac) references predavaci(sifra);
 
 alter table clanovi add foreign key (grupa) references grupe(sifra);
 alter table clanovi add foreign key (polaznik) references polaznici(sifra);
+
+
 
 
 
