b144a56e51710b357efda1393a4533014d111bc4
Ivan Tolić <>
2024-01-23 20:36:34 +0100

    popravljeni vanjski ključevi

--- teniska_liga.sql
+++ teniska_liga.sql
@@ -15,8 +15,8 @@ cijena decimal (18,2)not null,
 
 create table mecevi(
 id int not null primary key identity(1,1),
-izazivac  varchar(50) not null,
-izazvani varchar(50) not null,
+izazivac  int,
+izazvani int,
 pobjednik  varchar(50),
 datum datetime not null,
 red  char(10) not null,
@@ -36,17 +36,19 @@ clan bit not null,
 
 alter table mecevi add foreign key (sezona) references sezone(id);
 alter table mecevi add foreign key (natjecatelj) references natjecatelji(id);
+alter table mecevi add foreign key (izazivac) references natjecatelji(id);
+alter table mecevi add foreign key (izazvani) references natjecatelji(id);
 
 insert into sezone (pocetak_sezone,kraj_sezone,cijena)
 values ('2024-04-01 08:00:00', '2024-11-15 23:59:59', 100)
 
-insert into mecevi (izazivac, izazvani, datum, red)
+insert into mecevi (datum, red)
 values 
-('Valent Brkić', 'Tomislav Kušević', '2024-04-01 15:00:00', '5'),
-('Ljudevit Sukić', 'Ivan Salamon', '2024-04-01 16:30:00', '4'),
-('Antun Marošević', 'Ivan Tolić', '2024-04-01 18:00:00', '3'),
-('Dajana Tolić', 'Sara Matijević', '2024-04-01 19:30:00', '2'),
-('Tomislav Wilhelm', 'Jadranko Grubišić', '2024-04-01 21:00:00', '1');
+('2024-04-01 15:00:00', '5'),
+('2024-04-01 16:30:00', '4'),
+('2024-04-01 18:00:00', '3'),
+('2024-04-01 19:30:00', '2'),
+('2024-04-01 21:00:00', '1');
 
 insert into natjecatelji (ime, prezime, clan)
 values
