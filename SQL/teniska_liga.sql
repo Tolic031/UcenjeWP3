27aaa09366034d2a07c0ad1aee4b416c6c7845d7
Tolic031 <ivan.tolic1989@gmail.com>
2024-02-17 21:04:38 +0100

    dodana dva vanjska ključa u tablicu

--- SQL/teniska_liga.sql
+++ SQL/teniska_liga.sql
@@ -15,14 +15,13 @@ cijena decimal (18,2)not null,
 
 create table mecevi(
 id int not null primary key identity(1,1),
-izazivac  varchar(50) not null,
-izazvani varchar(50) not null,
-pobjednik  varchar(50),
+izazivac  int not null,
+izazvani int not null,
 datum datetime not null,
 red  char(10) not null,
 napomena text,
 sezona int,
-natjecatelj int
+pobjednik int
 );
 
 create table natjecatelji(
@@ -35,18 +34,12 @@ clan bit not null,
 );
 
 alter table mecevi add foreign key (sezona) references sezone(id);
-alter table mecevi add foreign key (natjecatelj) references natjecatelji(id);
+alter table mecevi add foreign key (pobjednik) references natjecatelji(id);
+alter table mecevi add foreign key (izazivac) references natjecatelji(id);
+alter table mecevi add foreign key (izazvani) references natjecatelji(id);
 
 insert into sezone (pocetak_sezone,kraj_sezone,cijena)
 values ('2024-04-01 08:00:00', '2024-11-15 23:59:59', 100)
-
-insert into mecevi (izazivac, izazvani, datum, red)
-values 
-('Valent Brkić', 'Tomislav Kušević', '2024-04-01 15:00:00', '5'),
-('Ljudevit Sukić', 'Ivan Salamon', '2024-04-01 16:30:00', '4'),
-('Antun Marošević', 'Ivan Tolić', '2024-04-01 18:00:00', '3'),
-('Dajana Tolić', 'Sara Matijević', '2024-04-01 19:30:00', '2'),
-('Tomislav Wilhelm', 'Jadranko Grubišić', '2024-04-01 21:00:00', '1');
 
 insert into natjecatelji (ime, prezime, clan)
 values
@@ -60,3 +53,12 @@ values
 ('Sara','Matijević', 2),
 ('Tomislav','Wilhelm', 1),
 ('Jadranko','Grubišić', 1);
+
+insert into mecevi (izazivac, izazvani, datum, red, pobjednik)
+values 
+(1,2,'2024-04-01 15:00:00', '5', null),
+(3,4,'2024-04-01 16:30:00', '4', null),
+(5,6,'2024-04-01 18:00:00', '3', 5),
+(7,8,'2024-04-01 19:30:00', '2', null),
+(9,10,'2024-04-01 21:00:00', '1', 10);
+
