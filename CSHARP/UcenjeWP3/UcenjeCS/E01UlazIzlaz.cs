f9715e461a819e38fbbc4e0dbc4b35350a6ab66a
Tolic031 <ivan.tolic1989@gmail.com>
2024-02-20 21:34:07 +0100

    Vježbanje kroz igru sa console write i console writeline.

--- CSHARP/UcenjeWP3/UcenjeCS/E01UlazIzlaz.cs
+++ CSHARP/UcenjeWP3/UcenjeCS/E01UlazIzlaz.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,12 +11,36 @@ namespace UcenjeCS
 
         public static void Izvedi()
         {
+            // pitati profa - Šta nam znači \r, kako staviti hrvatske dijakritičke znakove.
 
             string Ime;
             Console.Write("Unesi ime: ");
 
             Ime = Console.ReadLine();
             Console.WriteLine(Ime);
+
+
+            string Grad;
+            Console.Write("Unesi ime grada: ");
+            Grad = Console.ReadLine();
+            Console.WriteLine(Grad + " " + Grad + " " + Grad);
+
+            Console.Write("Edunova\n"); // - \n mi daje novi red ako nema consolewrite line
+            Console.WriteLine("\tOsijek");  // - \t mi daje tab - odvoji novu riječ za tab udaljenost.
+
+
+            Console.WriteLine("|\\---/|");
+            Console.WriteLine("| o_o |");
+            Console.WriteLine(" \\_^_/");
+
+
+            Console.WriteLine("      |\\      _,,,---,,_\r");
+            Console.WriteLine("ZZZzz /,`.-'`'    -.  ;-;;,_\r");
+            Console.WriteLine("     |,4-  ) )-,_. ,\\ (  `'-'\r");
+            Console.WriteLine("    '---''(_/--'  `-'\\_)  Felix Lee \r");
+    
+
+
         }
     }
 }
