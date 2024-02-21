96dd8b49ae7856800ea659ebed61c2b39beaf4fa
Tolic031 <ivan.tolic1989@gmail.com>
2024-02-21 08:13:42 +0100

    testiranje nove rute

--- CSHARP/UcenjeWP3/WebAPI/Controllers/E01UlazIzlaz.cs
+++ CSHARP/UcenjeWP3/WebAPI/Controllers/E01UlazIzlaz.cs
@@ -6,15 +6,26 @@ namespace WebAPI.Controllers
     [Route("E01")]
     public class E01UlazIzlaz:ControllerBase
     {
+        // ovdje počinje ruta
         [HttpGet]
         [Route("Hello")]
         public string HelloWorld(string Ime)
         {
             return "Dobro večer " + Ime;
         }
+        // ovdje završava ruta
 
 
 
+        // ovdje počinje ruta
+        [HttpGet]
+        [Route("zad1")]
+        public int Zad1(int prvi, int drugi)
+        {
+            
+            return prvi + drugi;
+        }
+        // ovdje završava ruta
 
     }
 }
