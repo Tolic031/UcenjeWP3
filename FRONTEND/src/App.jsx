73224cf35a22943a0500a3f10d4bedea0f50b25b
Tolic031 <ivan.tolic1989@gmail.com>
2024-04-02 07:58:17 +0200

    Početak frontenda

---
+++ FRONTEND/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import NavBar from './components/Navbar'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ FRONTEND/src/constants.js
@@ -0,0 +1,7 @@
+export const RoutesNames = {
+    HOME:'/',
+
+    SMJER_PREGLED : '/smjerovi',
+    SMJER_NOVI : '/smjerovi/dodaj',
+    SMJER_PROMJENI : '/smjerovi/:sifra'
+};
